a1b5af3bd8396af480c6c39c7060ea4b323e8d66
annierose2007 <142624392+annierose2007@users.noreply.github.com>
2024-04-16 10:18:01 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -1,25 +1,27 @@
 import java.util.ArrayList;
-public class OnlinePurchaseManager
-{
-  /** An ArrayList of purchased Gizmo objects,
-  * instantiated in the constructor.
-  */
+public class OnlinePurchaseManager{
   private ArrayList<Gizmo> purchases;
-  
-  /** Returns the number of purchased Gizmo objects that are electronic
-  * whose manufacturer is maker, as described in part (a).
-  */
-  public int countElectronicsByMaker(String maker)
-  {
-    /* to be implemented in part (a) */
+  public int countElectronicsByMaker(String maker){
+    int count = 0;
+    for (Gizmo gizmo : purchases) {
+      if (gizmo.isElectronic() && gizmo.getMaker().equals(maker)) {
+        count++;
+      }
+    }
+    return count;
   }
-  
-  /** Returns true if any pair of adjacent purchased Gizmo objects are
-  * equivalent, and false otherwise, as described in part (b).
-  */
-  public boolean hasAdjacentEqualPair()
-  {
-    /* to be implemented in part (b) */
+  public boolean hasAdjacentEqualPair(){
+    if (purchases.size() < 2) {
+      return false;
+    }
+    for (int i = 0; i < purchases.size() - 1; i++) {
+      Gizmo current = purchases.get(i);
+      Gizmo next = purchases.get(i + 1);
+      if (current.equals(next)) {
+        return true;
+      }
+    }
+    return false;
   }
   public OnlinePurchaseManager()
   {
